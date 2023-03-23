7253fd4abfc96cd7f7a2013694cebe8ca51752d9
Fred Tupas <fred.tupas21@gmail.com>
2023-03-23 19:29:41 +0100

    feat: add more deployments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -41,7 +41,7 @@ const config: HardhatUserConfig = {
     apiKey: {
       goerli: process.env.ETHERSCAN_API_KEY,
       sepolia: process.env.ETHERSCAN_API_KEY,
-      optimisticGoerli: process.env.ETHERSCAN_API_KEY,
+      optimisticGoerli: process.env.OPTIMISIC_API_KEY,
       gnosis: process.env.GNOSISSCAN_API_KEY,
       polygonMumbai: process.env.POLYGONSCAN_API_KEY,
     },
